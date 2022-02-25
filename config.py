a114a48206f9dfbd5f1ec119d4594e6dfcf1eff2
ernesto505e <100403117+ernesto505e@users.noreply.github.com>
2022-02-25 01:53:25 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from MoodleClient import MoodleClient
 
 BOT_TOKEN = '5010096091:AAE1Y9JfdqgcyCPRatkVzMgm_EVfCslxNxg'
 MAX_ZIP_SIZE = 100
-ACCES_USERS = ['obisoftdev','shapingdev']
-CREDENTIALS = {'username':'obysoft','password':'Obysoft2001@'}
+ACCES_USERS = ['obisoftdev','ernesto505']
+CREDENTIALS = {'username':'ernesto505','password':'48hNvYma*'}
 CACHE = {}
 
 def getCache():
